acb994bf4ef2d4d6612bd485e7ae9b4dd3b21aa1
Antonella Cino <antonella.cino18@gmail.com>
2020-04-07 17:33:43 +0200

    funzionamento invio messaggi da dettagliare

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
     var msg = "";
 
+    $('.footer input').click(
+        function() {
+            $('.fa-microphone').addClass('hidden');
+            $('.fa-telegram-plane').removeClass('hidden');
+        }
+    );
+
+    $('.fa-telegram-plane').click(
+        function() {
+            msg = $('.footer input').val();
+            console.log(msg);
+            $('.messages').append(msg);
+            $('.footer input').val("");
+            $('.fa-microphone').removeClass('hidden');
+            $('.fa-telegram-plane').addClass('hidden');
+        });
+
 
 
 });
