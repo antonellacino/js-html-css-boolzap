c3b31f670a81155e0eed843b06dba84d9c1d23b3
Antonella Cino <antonella.cino18@gmail.com>
2020-04-14 11:37:55 +0200

    correzioni

--- bonus/main.js
+++ bonus/main.js
@@ -50,8 +50,6 @@ $(document).ready(function() {
     searchInput.keyup(
         function() {
             var userText = searchInput.val().toUpperCase();
-            console.log(userText);
-
             contactList.each(
                 function() {
                     var name = $(this).find('span.fontBold').text().toUpperCase();
@@ -80,15 +78,13 @@ $(document).ready(function() {
 
     $(document).on("click", "i",
         function() {
-            var msgSelected = $(this).closest('li');
-            msgSelected.siblings('li').find('ul.delete').addClass('hidden');
-            msgSelected.find('ul.delete').toggleClass('hidden');
+            $(this).closest('li').find('ul.delete').toggle('hidden');
         }
     )
 
     $('.messagesContainer').on("click", "a.remove",
         function() {
-            $(this).closest('li.msg').remove();
+            $(this).closest('li.msg').hide();
         }
     )
 
@@ -96,7 +92,8 @@ $(document).ready(function() {
     //------------------------FUNZIONI-------------------------
     //funzione invio messaggi
     function sendMessage(message) {
-        $('.view ul.messagesList').append("<li class='sendMsg clear positionRelative msg'><p>" + msg + "<i class='fas fa-angle-down icons visibility'></i></p><p class='littleFont'>12:45</p><ul class='delete deleteRight hidden positionRight'><li><a href='#'>Info messaggio</a></li><li><a href='#' class='remove'>Cancella Messaggio</a></li></ul></li>");
+        takeTime();
+        $('.view ul.messagesList').append("<li class='sendMsg clear positionRelative msg'><p>" + msg + "<i class='fas fa-angle-down icons visibility'></i></p><p class='littleFont time'></p><ul class='delete deleteRight hidden positionRight'><li><a href='#'>Info messaggio</a></li><li><a href='#' class='remove'>Cancella Messaggio</a></li></ul></li>");
         msgInput.val("");
         microphoneIcons.removeClass('hidden');
         planeIcons.addClass('hidden');
@@ -105,10 +102,22 @@ $(document).ready(function() {
 
     //funzione x ricezione messaggi
     function receiptMessage() {
-        $('.view ul.messagesList').append("<li class='receiptMsg clear positionRelative msg'><p>ok<i class='fas fa-angle-down icons visibility'></i></p><p class='littleFont'>12:45</p><ul class='delete hidden'><li><a href='#'>Info messaggio</a></li><li><a href='#' class='remove'>Cancella Messaggio</a></li></ul></li>");
+        takeTime();
+        $('.view ul.messagesList').append("<li class='receiptMsg clear positionRelative msg'><p>ok<i class='fas fa-angle-down icons visibility'></i></p><p class='littleFont time'></p><ul class='delete hidden'><li><a href='#'>Info messaggio</a></li><li><a href='#' class='remove'>Cancella Messaggio</a></li></ul></li>");
     }
 
 
+    //funzione Time
+    function takeTime() {
+        var data = new Date();
+        var houres = data.getHours();
+        var minutes = data.getMinutes();
+        if (minutes < 10) {
+            $('.time').html(houres + ": 0" + minutes)
+        }
+        $('.time').html(" " + houres + ":" + minutes)
+
+    }
 
 
 
