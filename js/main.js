82906845d205997d15f1cc1a44d8ed54abce0313
Antonella Cino <antonella.cino18@gmail.com>
2020-04-15 15:09:51 +0200

    handlebars included

--- js/main.js
+++ js/main.js
@@ -7,8 +7,6 @@ $(document).ready(function() {
     var chat = $('.chat');
     var msg = "";
 
-    console.log(planeIcons);
-
     //status hover chat-list-------------------------------
     contactList.mouseenter(
         function() {
@@ -51,8 +49,6 @@ $(document).ready(function() {
     searchInput.keyup(
         function() {
             var userText = searchInput.val().toUpperCase();
-            console.log(userText);
-
             contactList.each(
                 function() {
                     var name = $(this).find('span.fontBold').text().toUpperCase();
@@ -63,7 +59,6 @@ $(document).ready(function() {
                     }
                 }
             )
-
         }
     );
     //Associo al nome della conversazione la propria chat
@@ -78,6 +73,7 @@ $(document).ready(function() {
             $('ul.delete').addClass('hidden');
         }
     );
+
 
     $(document).on("click", "i",
         function() {
@@ -98,7 +94,13 @@ $(document).ready(function() {
     //funzione invio messaggi
     function sendMessage(message) {
         var time = takeTime();
-        $('.view ul.messagesList').append("<li class='sendMsg clear positionRelative msg'><p>" + msg + "<i class='fas fa-angle-down icons visibility'></i></p><p class='littleFont'>" + time + "</p><ul class='delete deleteRight hidden positionRight'><li><a href='#'>Info messaggio</a></li><li><a href='#' class='remove'>Cancella Messaggio</a></li></ul></li>");
+
+        var source = $('#message-template').html();
+        var template = Handlebars.compile(source);
+        var context = { "sendReceipt": "sendMsg", "deleteRight": "deleteRight", "msg": msg, "time": time, };
+        var html = template(context);
+        $('.view ul.messagesList').append(html);
+        // $('.view ul.messagesList').append("<li class='sendMsg clear positionRelative msg'><p>" + msg + "<i class='fas fa-angle-down icons visibility'></i></p><p class='littleFont'>" + time + "</p><ul class='delete deleteRight hidden positionRight'><li><a href='#'>Info messaggio</a></li><li><a href='#' class='remove'>Cancella Messaggio</a></li></ul></li>");
         msgInput.val("");
         microphoneIcons.removeClass('hidden');
         planeIcons.addClass('hidden');
@@ -109,7 +111,12 @@ $(document).ready(function() {
     //funzione x ricezione messaggi
     function receiptMessage() {
         var time = takeTime();
-        $('.view ul.messagesList').append("<li class='receiptMsg clear positionRelative msg'><p>ok<i class='fas fa-angle-down icons visibility'></i></p><p class='littleFont'>" + time + "</p><ul class='delete hidden'><li><a href='#'>Info messaggio</a></li><li><a href='#' class='remove'>Cancella Messaggio</a></li></ul></li>");
+        var source = $('#message-template').html();
+        var template = Handlebars.compile(source);
+        var context = { "sendReceipt": "receiptMsg", "deleteRight": "none", "msg": "ok", "time": time };
+        var html = template(context);
+        $('.view ul.messagesList').append(html);
+        // $('.view ul.messagesList').append("<li class='receiptMsg clear positionRelative msg'><p>ok<i class='fas fa-angle-down icons visibility'></i></p><p class='littleFont'>" + time + "</p><ul class='delete hidden'><li><a href='#'>Info messaggio</a></li><li><a href='#' class='remove'>Cancella Messaggio</a></li></ul></li>");
         $('span.access').html("Ultimo accesso alle ore: " + time);
         contactList.filter('.active').find('div.log').text(time);
     }
